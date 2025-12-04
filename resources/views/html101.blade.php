3f2767a473f56c3aec8c020311aadd365e83b91a
Saran <sarannutchawong@gmail.com>
2025-12-04 19:15:38 +0700

    HTML01

---
+++ resources/views/html101.blade.php
@@ -0,0 +1,45 @@
+<!-- dddddddd -->
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <link rel = "stylesheet" href="css/bootstrap.css">
+        <title> Saran </title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Creepster&display=swap" rel="stylesheet">
+        <style>
+            body{
+                font-family: "Creepster", system-ui;
+            }
+        </style>
+    </head>
+
+    <body>
+        <div class="container mt-4">
+            <h1>Workshop #HTML - FORM</h1>
+            <form>
+                <div class="row mb-3 align-items-center">
+                    <div class="col-sm-2">
+                        <label for="fname" >Name</label>
+                    </div>
+                    <div class="col">
+                        <input id="fname" name="fname" type="text" class="form-control">
+                    </div>
+                </div>
+            </form>
+
+            <form>
+                <div class="row mb-3 align-items-center">
+                    <div class="col-sm-2">
+                        <label for="Pname" >Password</label>
+                    </div>
+                    <div class="col">
+                        <input id="Pname" class="form-control">
+                    </div>
+                </div>
+            </form>
+        </div>
+    </body>
+</html>
