19cd068f65a24af2c440fccf2161cc1ce1ed8225
Saran <sarannutchawong@gmail.com>
2025-12-06 02:20:48 +0700

    Update html101.blade.php

--- resources/views/html101.blade.php
+++ resources/views/html101.blade.php
@@ -8,10 +8,20 @@
         <title> Saran </title>
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Creepster&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Barriecito&family=Bungee+Spice&family=Titan+One&display=swap" rel="stylesheet">
         <style>
             body{
-                font-family: "Creepster", system-ui;
+                font-family: "Titan One", sans-serif;
+                margin: 0;
+                height: 100vh;
+        
+                background: radial-gradient(ellipse at bottom, #1b2735 0%, #090a0f 100%);
+                overflow: hidden;
+                color: #ffffff;
+                h1 {
+                    font-family: "Titan One", sans-serif;
+                    color:yellow;
+                }
             }
         </style>
     </head>
@@ -19,27 +29,136 @@
     <body>
         <div class="container mt-4">
             <h1>Workshop #HTML - FORM</h1>
+            <hr></hr>
             <form>
+                <!--- Name--->
                 <div class="row mb-3 align-items-center">
-                    <div class="col-sm-2">
+                    <div class="col-sm-1">
                         <label for="fname" >Name</label>
                     </div>
-                    <div class="col">
+                    <div class="col-sm-3">
                         <input id="fname" name="fname" type="text" class="form-control">
+                    </div>
+                </div>
+
+                <!---SurName--->
+                <div class="row mb-3 align-items-center">
+                    <div class="col-sm-1">
+                        <label for="Sname" >Surname</label>
+                    </div>
+                    <div class="col-sm-3">
+                        <input id="Sname" class="form-control">
+                    </div>
+                </div>
+
+                <!---BirthDay--->
+                <div class="row mb-3 align-items-center">
+                    <div class="col-sm-1">
+                        <label for="birthday">Birthday</label>
+                    </div>
+                    <div class="col-sm-3">
+                        <input id="birthday" name="birthday" type="date" class="form-control">
+                    </div>
+                </div>
+
+                <!---Gender--->
+                <div class="row mb-3 align-items-center">
+                    <div class="col-sm-1">
+                        <label for="favcolor">Gender</label>
+                    </div>
+                    <div class="col-sm-3">
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="radioDefault" id="radioDefault1">
+                            <label class="form-check-label" for="radioDefault1"> Male </label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="radioDefault" id="radioDefault2" checked>
+                            <label class="form-check-label" for="radioDefault2"> Female </label>
+                        </div>
+                    </div>
+                </div>
+                
+
+                <!---Image--->
+                <div class="row mb-3 align-items-center">
+                    <div class="col-sm-1">
+                        <label for="myfile">Image</label>
+                    </div>
+                    <div class="col-sm-3">
+                        <input  type="file" id = "myfile" name= "myfile" >
+                    </div>
+                </div>
+
+                <!--Address-->
+                <div class="row mb-3 align-items-center">
+                    <div class="col-sm-1">
+                        <label for="address">Address</label>
+                    </div>
+                    <div class="col-sm-3">
+                        <textarea id="address" name="address" rows="3" class="form-control"></textarea>
+                    </div>
+                </div>
+                
+                <!--Color-->
+                <div class="row mb-3 align-items-center">
+                    <div class="col-auto">
+                        <label for="favcolor">Faverite Color</label>
+                    </div>
+                    <div class="col-sm-3">
+                        <input type="color" id="favcolor" name="favcolor" class="form-control form-control-color">
+                    </div>
+                </div>
+                
+                <!--Music-->
+                <div class="row mb-3 align-items-center">
+                    <div class="col-auto">
+                        <label for="favcolor">Music Class</label>
+                    </div>
+                    <div class="col-sm-3">
+                        <div class="form-check1 form-check-inline">
+                            <input class="form-check1-input" type="radio" name="radioDefault1" id="radioDefault1">
+                            <label class="form-check1-label" for="radioDefault1"> String </label>
+                        </div>
+                        <div class="form-check1 form-check-inline">
+                            <input class="form-check1-input" type="radio" name="radioDefault1" id="radioDefault2" checked>
+                            <label class="form-check1-label" for="radioDefault2"> Pop </label>
+                        </div>
+                        <div class="form-check1 form-check-inline">
+                            <input class="form-check1-input" type="radio" name="radioDefault1" id="radioDefault3" checked>
+                            <label class="form-check1-label" for="radioDefault3"> Etc.</label>
+                        </div>
+                    </div>
+                </div>
+
+                <!--CheckBox-->
+                <div class="Check mb-3">
+                    <input class="Check-input" type="checkbox" value="" id="defaultCheck1">
+                    <label class="Check-label" for="defaultCheck1"> ยินดีให้เก็บข้อมูล </label>
+                </div>
+
+                <!--Success-->
+                <div class="row mb-3 align-items-center">
+                    <div class="col-auto">
+                        <button type="submit" class="btn btn-success" value = "submit">Success</button>
+                    </div>
+                    <div class="col-sm-3">
+                        <button type="reset" class="btn btn-light" value = "reset">Reset</button>
                     </div>
                 </div>
             </form>
 
-            <form>
-                <div class="row mb-3 align-items-center">
-                    <div class="col-sm-2">
-                        <label for="Pname" >Password</label>
-                    </div>
-                    <div class="col">
-                        <input id="Pname" class="form-control">
-                    </div>
-                </div>
-            </form>
+        <img src="https://miro.medium.com/v2/resize:fit:640/format:webp/1*OAXivevUVDA6ddtq-7jwVw.gif" class="floating-gif">
+        <style>
+            .floating-gif {
+                position: fixed;
+                right: 110px;        
+                top: 50%;
+                transform: translateY(-50%);   
+                width: 600px;       
+                z-index: 999;       
+            }
+        </style>
         </div>
+
     </body>
 </html>
